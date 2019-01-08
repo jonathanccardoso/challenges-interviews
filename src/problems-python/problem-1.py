dfb92a6b71b9289f51e667bf84e454dd3b3ce58f
jonathanccardoso <jonathan99moura@hotmail.com>
2019-01-08 14:30:13 -0300

    Add problem python and api-node

---
+++ src/problems-python/problem-1.py
@@ -0,0 +1,46 @@
+test = int(input())
+for i in range(0, test):
+  number = input()
+  if find('-'):
+  	
+
+'''
+- https://www.youtube.com/watch?v=gEbbso4XG00
+- https://www.youtube.com/watch?v=a7DH88vk2Sk
+
+'''
+
+'''
+Entrada de amostra
+
+    6
+    4123456789123456
+    5123-4567-8912-3456
+    61234-567-8912-3456
+    4123356789123456
+    5133-3367-8912-3456
+    5123 - 3567 - 8912 - 3456
+
+Saída de Amostra
+
+    Válido
+    Válido
+    Inválido
+    Válido
+    Inválido
+    Inválido
+'''
+
+
+'''
+
+Um cartão de crédito válido do ABCD Bank possui as seguintes características:
+
+► Deve começar com 4, 5 ou 6.
+► Deve conter exatamente 16 dígitos.
+► Deve consistir apenas em dígitos (0-9).
+► Pode ter dígitos em grupos de 4, separados por um hífen "-".
+► NÃO deve usar nenhum outro separador como '', '_', etc.
+► NÃO deve ter 4 ou mais dígitos repetidos consecutivos.
+
+'''
