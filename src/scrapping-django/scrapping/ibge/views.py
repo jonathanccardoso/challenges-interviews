abfe2c579ba0b167fe65b0127027dbb4b8f1f685
jonathanccardoso <jonathan99moura@hotmail.com>
2020-05-20 14:42:21 -0300

    Fixme: clean code on together arrays

--- src/scrapping-django/scrapping/ibge/views.py
+++ src/scrapping-django/scrapping/ibge/views.py
@@ -19,28 +19,16 @@ def index(request):
   natal_request = requests.get("https://www.ibge.gov.br/cidades-e-estados/rn/natal.html")
   natal_select = BeautifulSoup(natal_request.content, 'html.parser')
 
-  natal_ul = natal_select.find(class_='resultados-padrao')
-  natal_div = natal_ul.findAll("div", {"class": "indicador"})
-  
+  natal_ul = natal_select.find(class_='resultados-padrao')  
   natal_label = natal_ul.findAll("p", {"class": "ind-label"})
   natal_value = natal_ul.findAll("p", {"class": "ind-value"})
 
   natal_data = []
-  cont_label = 0
-  cont_value = 0
-  for label in natal_label:
+  for label, value in zip(natal_label, natal_value):
     text_label = label.text
+    text_value = value.text
     if text_label.split()[0] == fields[0] or text_label.split()[0] == fields[1]:
-      print(cont_label) # positions
-      
-      # tenho que por na possição que a label foi intercepitada!
-      for value in natal_value:
-        text_value = value.text
-        if cont_value == cont_label:
-          natal_data.append([text_label, text_value])
-        cont_value += 1
-    cont_label += 1
-    cont_value = 0
+      natal_data.append([text_label, text_value])
 
   context = { 
     "natal_data" : natal_data, 
