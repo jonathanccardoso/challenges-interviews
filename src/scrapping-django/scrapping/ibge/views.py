164152a17c81214fabd2593d6976669df7321475
jonathanccardoso <jonathan99moura@hotmail.com>
2020-05-20 17:32:37 -0300

    Fixme: adds function to view city data

--- src/scrapping-django/scrapping/ibge/views.py
+++ src/scrapping-django/scrapping/ibge/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 import re
 
-import requests
+import requests, json
 from bs4 import BeautifulSoup
 
 
@@ -16,22 +16,39 @@ def index(request):
     'IDHM'
   ]
 
-  natal_request = requests.get("https://www.ibge.gov.br/cidades-e-estados/rn/natal.html")
-  natal_select = BeautifulSoup(natal_request.content, 'html.parser')
+  def cityData(city):
+    city_data = []
+    
+    for item in city:
+      # convert and read in json
+      item = json.dumps(item)
+      item = json.loads(item)
+      
+      name = item['name']
+      state = item['state']
 
-  natal_ul = natal_select.find(class_='resultados-padrao')  
-  natal_label = natal_ul.findAll("p", {"class": "ind-label"})
-  natal_value = natal_ul.findAll("p", {"class": "ind-value"})
+      url_request = requests.get("https://www.ibge.gov.br/cidades-e-estados/"+ state +"/"+ name +".html")
+      div_select = BeautifulSoup(url_request.content, 'html.parser')
 
-  natal_data = []
-  for label, value in zip(natal_label, natal_value):
-    text_label = label.text
-    text_value = value.text
-    if text_label.split()[0] == fields[0] or text_label.split()[0] == fields[1]:
-      natal_data.append([text_label, text_value])
+      ul = div_select.find(class_='resultados-padrao')  
+      city_label = ul.findAll("p", {"class": "ind-label"})
+      city_value = ul.findAll("p", {"class": "ind-value"})
+      
+      data = []
+      for label, value in zip(city_label, city_value):
+        text_label = label.text
+        text_value = value.text
+        if text_label.split()[0] == fields[0] or text_label.split()[0] == fields[1]:
+          data.append([text_label, text_value])
+      data.append([name, state])
 
-  context = { 
-    "natal_data" : natal_data, 
+      city_data.append(data)
+    return city_data
+
+  data = cityData(city)
+
+  context = {
+    "data": data
   } 
 
   return render(request, 'ibge/index.html', context)
